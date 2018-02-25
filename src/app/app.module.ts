613c11ff1be52ab65010aadf7b83f5d9db7b73c2
Robert Stine <robwstine@outlook.com>
2018-02-25 15:42:54 -0500

    AssignmentTwo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+  userName = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isButtonDisabled() {
+    return (this.userName === '');
+  }
+
+  clearName() {
+    this.userName = '';
+  }
+}
